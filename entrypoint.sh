9ab8eb07e3ca34cb01953e4503e800ffcf11d55b
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2020-08-04 17:09:51 +0100

    key debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,9 @@
 set -ex
 
 ssh-agent
-
+echo "prekey"
+echo $SSH_PRIVATE_KEY
+echo "postkey"
 ssh-add - <<< "$SSH_PRIVATE_KEY"
 
 git-filter-repo --path=lib/{includes,tests/phpunit}/{c,C}hanges/ --path lib/packages/wikibase/changes/ --path .mailmap --path-rename=lib/includes/{c,C}hanges:src --path-rename=lib/tests/phpunit/{c,C}hanges:tests --path-rename lib/packages/wikibase/changes/: --message-callback 'return re.sub(b"^changes: ", b"", message)'
