fcd60b5ed158fcc7650bf8455f9c83bac7e72012
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2020-08-04 16:44:33 +0100

    add user

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@ set
 
 git-filter-repo --path=lib/{includes,tests/phpunit}/{c,C}hanges/ --path lib/packages/wikibase/changes/ --path .mailmap --path-rename=lib/includes/{c,C}hanges:src --path-rename=lib/tests/phpunit/{c,C}hanges:tests --path-rename lib/packages/wikibase/changes/: --message-callback 'return re.sub(b"^changes: ", b"", message)'
 
-git remote add tarrow "https://$API_TOKEN_GITHUB@github.com/tarrow/Changes-clone.git"
+git remote add tarrow "https://tarrow:$API_TOKEN_GITHUB@github.com/tarrow/Changes-clone.git"
 git push tarrow HEAD:main
