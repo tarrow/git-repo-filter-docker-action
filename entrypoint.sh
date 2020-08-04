44e14cc11536705d9f3e610e713dfd8855007044
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2020-08-04 17:27:38 +0100

    more ssh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,11 @@
 set -ex
 
 mkdir -p ~/.ssh
-ssh-keyscan github.com >> ~/.ssh/known_hosts
+ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 eval `ssh-agent -s`
 ssh-add - <<< "$SSH_PRIVATE_KEY"
+
+ssh-add -l
 
 git-filter-repo --path=lib/{includes,tests/phpunit}/{c,C}hanges/ --path lib/packages/wikibase/changes/ --path .mailmap --path-rename=lib/includes/{c,C}hanges:src --path-rename=lib/tests/phpunit/{c,C}hanges:tests --path-rename lib/packages/wikibase/changes/: --message-callback 'return re.sub(b"^changes: ", b"", message)'
 
