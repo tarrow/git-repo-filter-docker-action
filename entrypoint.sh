10208c7e083b0d1d7587c1a60c7db4469f6a83a0
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2020-08-04 14:29:22 +0100

    init

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+git-filter-repo --path=lib/{includes,tests/phpunit}/{c,C}hanges/ --path lib/packages/wikibase/changes/ --path .mailmap --path-rename=lib/includes/{c,C}hanges:src --path-rename=lib/tests/phpunit/{c,C}hanges:tests --path-rename lib/packages/wikibase/changes/: --message-callback 'return re.sub(b"^changes: ", b"", message)'
+
+git remote add tarrow https://github.com/tarrow/Changes-clone.git
+git push tarrow HEAD:main
