d094ada881f02c4707bc16f72d23a636efff79bd
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2020-08-04 16:56:47 +0100

    try ssh key again

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -ex
 
+ssh-agent
+
+ssh-add - <<< "$SSH_PRIVATE_KEY"
+
 git-filter-repo --path=lib/{includes,tests/phpunit}/{c,C}hanges/ --path lib/packages/wikibase/changes/ --path .mailmap --path-rename=lib/includes/{c,C}hanges:src --path-rename=lib/tests/phpunit/{c,C}hanges:tests --path-rename lib/packages/wikibase/changes/: --message-callback 'return re.sub(b"^changes: ", b"", message)'
 
-git remote add tarrow "https://tarrow:$API_TOKEN_GITHUB@github.com/tarrow/Changes-clone.git"
+git remote add tarrow "git@github.com:tarrow/Changes-clone.git"
 git push tarrow HEAD:main
