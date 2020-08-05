d6bbe9726ca5711e9dbba3e7b63010c3984af47a
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2020-08-05 16:39:16 +0100

    use @

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,7 @@ set -ex
 ssh-keyscan -t rsa github.com >> /etc/ssh/ssh_known_hosts
 eval `ssh-agent -s`
 ssh-add - <<< "$SSH_PRIVATE_KEY"
-
-echo "$@"
-
-git-filter-repo $1
+git-filter-repo $@
 
 git remote add remotetarget "git@github.com:$TARGET_ORG/$TARGET_REPO.git"
 git push remotetarget HEAD:"$TARGET_BRANCH"
