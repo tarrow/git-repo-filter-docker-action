55e39533d2e105ce4d0fe4d7a34ec0a624fe2b2a
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2020-08-05 14:16:10 +0100

    parameterise

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ ssh-add - <<< "$SSH_PRIVATE_KEY"
 
 ssh-add -l
 
-git-filter-repo --path=lib/{includes,tests/phpunit}/{c,C}hanges/ --path lib/packages/wikibase/changes/ --path .mailmap --path-rename=lib/includes/{c,C}hanges:src --path-rename=lib/tests/phpunit/{c,C}hanges:tests --path-rename lib/packages/wikibase/changes/: --message-callback 'return re.sub(b"^changes: ", b"", message)'
+git-filter-repo "$@"
 
-git remote add tarrow "git@github.com:tarrow/Changes-clone.git"
-git push tarrow HEAD:main
+git remote add remotetarget "git@github.com:$TARGET_ORG/$TARGET_REPO.git"
+git push remotetarget HEAD:$TARGET_BRANCH
